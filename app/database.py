42c55d6824dc3c5b85a6e7ccec5102556a69fe06
arnob550 <abrarshahrierarnab@gmail.com>
2026-02-15 19:40:29 +0600

    10 th

--- app/database.py
+++ app/database.py
@@ -1,17 +1,22 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = "sqlite:///./studyplanner.db"
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
